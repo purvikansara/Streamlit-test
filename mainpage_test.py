17527ce9dac7033288bd0fa4e765bb1edeb01a49
pk <purvikansara@gmail.com>
2024-01-18 11:28:30 -0800

    multipagetest

---
+++ mainpage_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# optional st.set_page_config() is used to configure the settings for the Streamlit app's page.
+st.set_page_config()
+
+st.image('Images/money_tree.jpg')
+st.title('Choose page on sidebar:')
+st.subheader('Examine the training data')
+st.subheader('or')
+st.subheader('Either use and explore the model')
