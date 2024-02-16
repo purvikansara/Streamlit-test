73b74774b014c6242ae76881c2cb08a67168f9ca
pk <purvikansara@gmail.com>
2024-02-15 17:34:29 -0800

    working on error with image

--- mainpage_test.py
+++ mainpage_test.py
@@ -1,10 +1,29 @@
 import streamlit as st
 
-# optional st.set_page_config() is used to configure the settings for the Streamlit app's page.
-st.set_page_config()
+# # optional st.set_page_config() is used to configure the settings for the Streamlit app's page.
+# st.set_page_config()
 
-st.image('Images/money_tree.jpg')
-st.title('Choose page on sidebar:')
-st.subheader('Examine the training data')
-st.subheader('or')
-st.subheader('Either use and explore the model')
+# #st.image('money_tree.jpg')
+# st.title('Choose page on sidebar:')
+# st.subheader('Examine the training data')
+# st.subheader('or')
+# st.subheader('Either use and explore the model')
+
+
+import streamlit as st
+from PIL import Image
+
+def main():
+    st.title("Streamlit Image ")
+
+    # Upload image through Streamlit UI
+    uploaded_image = st.file_uploader("money_tree.jpg", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        # Display the uploaded image
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+        # Additional processing or analysis with the image can be added here
+
+if __name__ == "__main__":
+    main()
