f71e69a27bac30cc178efed1333941141a78844d
pk <purvikansara@gmail.com>
2024-04-08 14:38:49 -0700

    Update mainpage_test.py

--- mainpage_test.py
+++ mainpage_test.py
@@ -13,22 +13,22 @@ st.write("Original Image ")
 st.image(image)
 
 
-# st.subheader('Examine the training data')
-# st.subheader('or')
-# st.subheader('Either use and explore the model')
+st.subheader('Examine the training data')
+st.subheader('or')
+st.subheader('Either use and explore the model')
 
 
-# def main():
-#     st.title("Streamlit Image ")
+def main():
+    st.title("Streamlit Image ")
 
-#     # Upload image through Streamlit UI
-#     uploaded_image = st.file_uploader("money_tree.jpg", type=["jpg", "jpeg", "png"])
+    # Upload image through Streamlit UI
+    uploaded_image = st.file_uploader("money_tree.jpg", type=["jpg", "jpeg", "png"])
 
-#     if uploaded_image is not None:
-#         # Display the uploaded image
-#         st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+    if uploaded_image is not None:
+        # Display the uploaded image
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
-#         # Additional processing or analysis with the image can be added here
+        # Additional processing or analysis with the image can be added here
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
